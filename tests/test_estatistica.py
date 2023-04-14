9d38c50337612fe05f0ac7317c9d84b11d452107
gomesluiz <gomes.luiz@gmail.com>
2023-04-14 17:39:32 -0300

    initial commit

--- tests/test_estatistica.py
+++ tests/test_estatistica.py
@@ -1,30 +1,26 @@
 from numpy.random import seed
 from numpy.random import randint
 
+import pytest
+
 from pyestat.estatistica import Estatistica
 
-class TestEstatistica:
-
-    def test_calcula_media_caminho_1(self):
-        assert Estatistica.calcule_media([10, 5, 15, -999], 5, 20) == 10
-
-
-    def test_calcula_media_caminho_2(self):
-        seed(1)
-        assert Estatistica.calcule_media(randint(10, 150, 100), 0, 5) == 0
+# garante o determinismo do teste.
+seed(1)
+valores = randint(10, 50, 100)
 
 
-    def test_calcula_media_caminho_3(self):
-        assert Estatistica.calcule_media([-999], 0, 50) == 0
-
-
-    def test_calcula_media_caminho_4(self):
-        assert Estatistica.calcule_media([5, 10, 20, -999], 10, 50) == 15
-
-
-    def test_calcula_media_caminho_5(self):
-        assert Estatistica.calcule_media([10, 40, 100, -999], 10, 50) == 25
-
-    def test_calcula_media_caminho_6(self):
-        valores = randint(10, 50, 100)
-        assert Estatistica.calcule_media(valores, 10, 50) == sum(valores)/100
+@pytest.mark.parametrize(
+    "valores, minimo, maximo, media",
+    [
+        ([10, 5, 15, -999], 5, 20, 10),
+        (randint(10, 150, 100), 0, 5, 0),
+        ([-999], 0, 50, 0),
+        ([5, 10, 20, -999], 10, 50, 15),
+        ([10, 40, 100, -999], 10, 50, 25),
+        (valores, 10, 50, sum(valores) / 100),
+    ],
+)
+def test_calcule_media(valores, minimo, maximo, media):
+    """Testa o metodo calcule a média"""
+    assert Estatistica.calcule_media(valores, minimo, maximo) == media
